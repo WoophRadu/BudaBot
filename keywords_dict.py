3508adb64f0dfab79f779e4bcafbf8da2f54b55b
Wooph Radu <radu.wooph@gmail.com>
2017-12-21 20:12:36 +0200

    kwords update

--- keywords_dict.py
+++ keywords_dict.py
@@ -2,5 +2,9 @@ kwords={
     "cristi": "cristi, adica cel mai tare?",
     "ok": "ok, mai bine doua chei",
     "dezintegrez": "dezintegrez, bine ca nu dez-char-egrez",
-    "critica": "critica, bine ca nu patrunzatoare"
+    "critica": "critica, bine ca nu patrunzatoare",
+    "interesant": "interesant, bine ca nu char-eresant",
+    "lasama in pace": "bine ca nu lasa-ma in razboi",
+    "lasa-ma in pace": "bine ca nu lasa-ma in razboi",
+    "pascal": "pascal, bine ca nu pas-iapa",
 }
